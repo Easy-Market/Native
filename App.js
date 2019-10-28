be6d21a4877f1e4c62a087f16168d9f5f179a62e
basilcea <ogbonnabasil3@gmail.com>
2019-10-28 22:37:41 +0100

    change from expo to andriod emeulator

--- App.js
+++ App.js
@@ -10,8 +10,8 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.welcome}>Easy Market</Text>
+        <Text style={styles.instructions}>Nigerian Top Reseller Platform npm</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
