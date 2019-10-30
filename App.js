9107b1fc0be0139270a68f89ea1add3cc4a46447
basilcea <ogbonnabasil3@gmail.com>
2019-10-30 14:32:53 +0100

    set up home page

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 import Splash from './views/splash'
+import Home  from './views/home'
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
@@ -8,7 +9,7 @@ const instructions = Platform.select({
 
 export default App = () => {
     return (
-      <Splash />
+      <Home />
     );
   }
 
--- views/home.js
+++ views/home.js
@@ -1,24 +1,85 @@
-import React from 'react';
-import {Text, View, Dimensions, ImageBackground , StyleSheet} from 'react-native';
-import styled from styled-components;
-import {FaGoogle , FaFacebook} from 'react-icons/fa'
+import React, { useState, useEffect } from "react";
+import {
+  Text,
+  View,
+  TouchableHighlight,
+  Dimensions,
+  ImageBackground,
+  StyleSheet
+} from "react-native";
+import styled from "styled-components";
+import background from "../assests/background.jpg";
 
+let height;
+let width;
+const getDimensions = () => {
+    dims = Dimensions.get("window");
+    return dims;
+  };
+height = getDimensions().height
+width = getDimensions().width
+const Home = props => {
+  const [portrait, setPortrait] = useState(true);
 
-const Home = (props) => {
-    return (
-        <Container>
-        <ImageBackground source={}  style={otherStyle.background}>
-        <Heading>EasyMarket</Heading>
-        <Slogan>Sell Easy , Buy Easy</Slogan>
-        <Login>
-            <FaGoogle />
-            <FaFacebookF />
-        </Login>
-        </ImageBackground>
-        </Container>
-    )
-} 
+  useEffect(() => {
+    getDimensions();
+    return Dimensions.removeEventListener("change", () => getDimensions());
+  }, Dimensions.addEventListener("change", () => getDimensions()));
 
-const Container = styled.View`
+  const changeOrientation = () => {
+    getDimensions().width > getDimensions().height
+      ? setPortrait(false)
+      : setPortrait(true);
+  };
+  const getStyle = () =>
+    portrait ? OtherStyles.portStyle : OtherStyles.landScapeStyle;
 
-`
+  return (
+    <ImageBackground
+      source={background}
+      style={{
+        ...OtherStyles.background,
+        width: width,
+        height: height
+      }}
+      imageStyle={getStyle()}
+      onLayout={changeOrientation}
+    >
+      <Heading>EasyMarket</Heading>
+      <Slogan>Sell Easy , Buy Easy</Slogan>
+      <TouchableHighlight>
+          <View>
+              <Text>Continue</Text>
+          </View>
+      </TouchableHighlight>
+    </ImageBackground>
+  );
+};
+export default Home;
+
+const Heading = styled.Text`
+  color: #912f40;
+  font-family: "lora-italic";
+  font-size: ${width / 10};
+  margin-top:${height / 3};
+`;
+const Slogan = styled.Text`
+  color: #080705;
+  text-align: center;
+  font-size: ${width / 20};
+`;
+const OtherStyles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  landScapeStyle: {
+    width: width,
+    height
+  },
+  portStyle: {
+    height: height / 2,
+    width,
+  }
+});
